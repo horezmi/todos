5636ab6b2e374f345622090d5b370d40f1a91946
Aykhan <a.abdullazade@vpene.ru>
2021-04-17 16:23:56 +0300

    useState for done

--- src/components/TodoListItem/index.tsx
+++ src/components/TodoListItem/index.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import './index.scss'
+import React, { useState } from "react";
+import "./index.scss";
 
+const TodoListItem = ({ label, important }: any) => {
+  const [done, setDone] = useState(false);
+  let spanClasses = "todo-list-item__label";
 
-const TodoListItem = ({ label, important } : any) => {
-    const spanStyle = {
-        color: important ? 'tomato' : 'black'
-    }
-    
-    return (
-        <div className="todo-list-item">
-            <span 
-                style={spanStyle}>{ label }
-            </span>
+  if (important) spanClasses += " important";
+  if (done) spanClasses += " done";
+  const labelClickHandler = () => {
+      setDone(!done);
+  };
 
-            <div className="todo-list-item__btns">
-                <button className="btn btn-danger">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <button className="btn btn-info">
-                    <i className="fas fa-info"></i>
-                </button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="todo-list-item">
+      <span onClick={labelClickHandler} className={spanClasses}>
+        {label}
+      </span>
+
+      <div className="todo-list-item__btns">
+        <button className="btn btn-danger">
+          <i className="fas fa-trash"></i>
+        </button>
+        <button className="btn btn-info">
+          <i className="fas fa-info"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TodoListItem;

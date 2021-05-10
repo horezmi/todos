cef660dac9ce4be4db0289aebb9412b8349839b8
Aykhan <aykhan.abdullazade@gmail.com>
2021-05-11 01:56:49 +0300

    delete bt =)

--- src/components/ItemStatusCounters/index.tsx
+++ src/components/ItemStatusCounters/index.tsx
@@ -6,7 +6,6 @@ const ItemStatusCounters = ({ activeCount, doneCount }: any) => {
   return (
     <div className="item-status-counters">
       <span>{activeCount} active</span>
-      <br />
       <span>{doneCount} done</span>
     </div>
   );

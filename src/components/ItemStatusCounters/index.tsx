903b86e7c52d54c2c1c13a809a268d38ce969025
Aykhan <aykhan.abdullazade@gmail.com>
2021-05-13 23:29:52 +0300

    fix

--- src/components/ItemStatusCounters/index.tsx
+++ src/components/ItemStatusCounters/index.tsx
@@ -4,16 +4,8 @@ import './index.scss';
 
 const ItemStatusCounters = ({ activeCount, doneCount }: any) => (
   <div className="item-status-counters">
-    <span>
-      {activeCount}
-      {' '}
-      active
-    </span>
-    <span>
-      {doneCount}
-      {' '}
-      done
-    </span>
+    <span>{activeCount} active</span>
+    <span>{doneCount} done</span>
   </div>
 );
 
